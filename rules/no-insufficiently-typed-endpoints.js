e6cd214b5f7228befe394aa2e28d370bbd296737
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-10-12 09:21:35 +0700

    feat: improve no-insufficiently-typed-endpoints rule

--- rules/no-insufficiently-typed-endpoints.js
+++ rules/no-insufficiently-typed-endpoints.js
@@ -3,13 +3,16 @@ module.exports = {
     type: "suggestion",
     docs: {
       description:
-        "Require @ApiOperation and @ApiResponse for HTTP method decorators (@Get, @Post, @Put, @Patch, @Delete)",
+        "Ensure every HTTP endpoint is properly annotated for API client generation",
       category: "Best Practices",
       recommended: true,
     },
     messages: {
       missingApiOperation: "Method is missing the @ApiOperation decorator.",
       missingApiResponse: "Method is missing the @ApiResponse decorator.",
+      emptySummary: "@ApiOperation must have a non-empty summary.",
+      emptyStatus: "@ApiResponse must have a non-empty status key.",
+      emptyType: "@ApiResponse must have a non-empty type key.",
     },
     schema: [], // no options
   },
@@ -25,26 +28,68 @@ module.exports = {
       });
 
       if (hasHttpMethodDecorator) {
-        const hasApiOperation = decorators.some((decorator) => {
+        const apiOperationDecorator = decorators.find((decorator) => {
           return decorator.expression.callee.name === "ApiOperation";
         });
 
-        const hasApiResponse = decorators.some((decorator) => {
+        const apiResponseDecorator = decorators.find((decorator) => {
           return decorator.expression.callee.name === "ApiResponse";
         });
 
-        if (!hasApiOperation) {
+        if (!apiOperationDecorator) {
           context.report({
             node,
             messageId: "missingApiOperation",
           });
+        } else {
+          // Check if the summary exists and is non-empty
+          const summaryProperty =
+            apiOperationDecorator.expression.arguments[0]?.properties.find(
+              (prop) => prop.key.name === "summary"
+            );
+
+          if (!summaryProperty || summaryProperty.value.value.trim() === "") {
+            context.report({
+              node: apiOperationDecorator,
+              messageId: "emptySummary",
+            });
+          }
         }
 
-        if (!hasApiResponse) {
+        if (!apiResponseDecorator) {
           context.report({
             node,
             messageId: "missingApiResponse",
           });
+        } else {
+          // Check if the status exists and is non-empty
+          const statusProperty =
+            apiResponseDecorator.expression.arguments[0]?.properties.find(
+              (prop) => prop.key.name === "status"
+            );
+
+          if (
+            !statusProperty ||
+            typeof statusProperty.value.value !== "number"
+          ) {
+            context.report({
+              node: apiResponseDecorator,
+              messageId: "emptyStatus",
+            });
+          }
+
+          // Check if the type exists and is non-empty
+          const typeProperty =
+            apiResponseDecorator.expression.arguments[0]?.properties.find(
+              (prop) => prop.key.name === "type"
+            );
+
+          if (!typeProperty || !typeProperty.value) {
+            context.report({
+              node: apiResponseDecorator,
+              messageId: "emptyType",
+            });
+          }
         }
       }
     }
