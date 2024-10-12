b1b9452ed7e8c57148bdf104403cb079c7a802de
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-10-12 09:45:10 +0700

    feat: export recommended config

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ module.exports = {
   rules: {
     "no-insufficiently-typed-endpoints": require("./rules/no-insufficiently-typed-endpoints"),
     "no-missing-api-bearer-auth": require("./rules/no-missing-api-bearer-auth"),
+    "enforce-api-route-kebab-case": require("./rules/enforce-api-route-kebab-case"),
+    "enforce-controller-api-tags": require("./rules/enforce-controller-api-tags"),
+  },
+  configs: {
+    recommended: {
+      rules: {
+        "no-insufficiently-typed-endpoints": "error",
+        "no-missing-api-bearer-auth": "error",
+        "enforce-api-route-kebab-case": "error",
+        "enforce-controller-api-tags": "error",
+      },
+    },
   },
 };
